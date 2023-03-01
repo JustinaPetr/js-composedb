0aefe4f3f41559dd06b8c9af60d119f0a8860ab0
Paul Le Cam <paul@3box.io>
2023-03-01 10:17:06 +0000

    Use remote IPFS setup

--- packages/cli/globalSetup.js
+++ packages/cli/globalSetup.js
@@ -1,26 +1,40 @@
-import { setup } from 'jest-dev-server'
+import { join } from 'node:path'
 import { Model } from '@ceramicnetwork/stream-model'
 import fs from 'fs-extra'
-import { join } from 'node:path'
+import ipfsBin from 'go-ipfs'
+import { createController } from 'ipfsd-ctl'
+import { setup } from 'jest-dev-server'
+import * as kuboRpcModule from 'kubo-rpc-client'
 import { dir } from 'tmp-promise'
+
+async function getIPFS() {
+  return await createController({
+    args: ['--enable-pubsub-experiment'],
+    disposable: true,
+    ipfsBin: ipfsBin.path(),
+    kuboRpcModule,
+  })
+}
 
 async function getPaths() {
   const tempDir = await dir({ unsafeCleanup: true })
   const tempPath = tempDir.path
-  const configPath = join(tempDir.path, 'config')
+
+  const configPath = join(tempPath, 'config')
+  const stateStoreDir = join(tempPath, 'statestore')
+  await Promise.all([fs.ensureDir(configPath), fs.ensureDir(stateStoreDir)])
+
   return {
     tempDir: tempPath,
-    configDir: configPath,
+    stateStoreDir,
     configFile: join(configPath, 'daemon.config.json'),
-    stateStoreDir: join(tempPath, 'statestore'),
-    ipfsDir: join(tempPath, 'goipfs'),
     indexingDB: join(tempPath, 'indexing.sqlite'),
   }
 }
 
 // Create the custom config here to make sure that we start with a clear indexing db setup each time
 // TODO: Investigate why the indexing API throws errors when we run tests several times without cleaning the db
-function getConfig(paths) {
+function getConfig(paths, ipfs) {
   return {
     anchor: {},
     'http-api': {
@@ -30,7 +44,7 @@ function getConfig(paths) {
       ],
       'cors-allowed-origins': [new RegExp('.*')],
     },
-    ipfs: { mode: 'bundled' },
+    ipfs: { mode: 'remote', host: ipfs },
     logger: { 'log-level': 2, 'log-to-files': false },
     metrics: {
       'metrics-exporter-enabled': false,
@@ -51,21 +65,18 @@ function getConfig(paths) {
 }
 
 export default async function globalSetup() {
-  const paths = await getPaths()
+  const [controller, paths] = await Promise.all([getIPFS(), getPaths()])
+
+  globalThis.IPFS_CONTROLLER = controller
   globalThis.TEMP_DIR_PATH = paths.tempDir
 
-  // Create all of the dirs required for testing
-  await Promise.all([
-    fs.ensureDir(paths.configDir),
-    fs.ensureDir(paths.stateStoreDir),
-    fs.ensureDir(paths.ipfsDir),
-  ])
-
   // Write the config file so that its path can be passed to deamon below
-  await fs.writeJson(paths.configFile, getConfig(paths))
+  const { address, port } = controller.apiAddr.nodeAddress()
+  const config = getConfig(paths, `http://${address}:${port}`)
+  await fs.writeJson(paths.configFile, config)
 
   await setup({
-    command: `IPFS_PATH=\'${paths.ipfsDir}\' ./node_modules/.bin/ceramic daemon --config ${paths.configFile}`,
+    command: `./node_modules/.bin/ceramic daemon --config ${paths.configFile}`,
     debug: true,
     launchTimeout: 240000,
     port: 7007,
--- packages/cli/globalTeardown.js
+++ packages/cli/globalTeardown.js
@@ -2,6 +2,9 @@ import { teardown } from 'jest-dev-server'
 import fs from 'fs-extra'
 
 export default async function globalTeardown() {
-  await fs.rm(globalThis.TEMP_DIR_PATH, { force: true, recursive: true })
+  await Promise.all([
+    globalThis.IPFS_CONTROLLER.stop(),
+    fs.rm(globalThis.TEMP_DIR_PATH, { force: true, recursive: true }),
+  ])
   await teardown()
 }
